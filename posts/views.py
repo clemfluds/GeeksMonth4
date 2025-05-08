74aee7a837af9eba86db2252609ba7d001ae60cf
clemfluds <danielzakat@inbox.ru>
2025-05-08 20:13:54 +0600

    CompletedHW2

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from random import randint
+from posts.models import Post
 
-def index(request):
-    return HttpResponse (f"Hello world.{randint(0, 100)}")
+def homepage_view(request):
+    return render(request, "base.html")
 
-
-def html_view(request):
-    return render(request, "test.html")
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/posts.html", context={"posts": posts})
